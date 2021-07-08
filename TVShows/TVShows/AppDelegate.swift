2b8130f8abf8293b38429677e733c21c5e3e24af
Bernard Ibrovic <ibrovic.bernard558@gmail.com>
2021-07-08 18:58:27 +0200

    Xcode project setup

---
+++ TVShows/TVShows/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  TVShows
+//
+//  Created by Bernard Ibrovic on 08.07.2021..
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
