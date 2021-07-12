b532e98ac7130efa248e972c1cfec257c38af5c9
Bernard Ibrovic <ibrovic.bernard558@gmail.com>
2021-07-12 18:02:03 +0200

    Add first cocoapods

--- TVShows/TVShows/ViewController.swift
+++ TVShows/TVShows/ViewController.swift
@@ -6,12 +6,23 @@
 //
 
 import UIKit
+import SVProgressHUD
 
-class ViewController: UIViewController {
-
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("Test")
+        self.titleLabel.text = "This is my second app"
+        SVProgressHUD.show()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+            SVProgressHUD.dismiss()
+        }
+        
     }
 
 
