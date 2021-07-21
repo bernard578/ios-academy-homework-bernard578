00406d9525a38f083be3d75170e2b1ac1c31a78f
Bernard Ibrovic <ibrovic.bernard558@gmail.com>
2021-07-21 21:19:11 +0200

    Revert "Add Home.storyboard and HomeViewController"
    This reverts commit f35afb1ad71437357e4d709bcaeba1e3c7bae636.

--- TVShows/TVShows/Login/LoginViewController.swift
+++ TVShows/TVShows/Login/LoginViewController.swift
@@ -26,22 +26,4 @@ final class LoginViewController: UIViewController {
         }
         sender.isSelected = !sender.isSelected
     }
-    
-    
-    @IBAction func touchLoginButtonActionHandler(_ sender: UIButton) {
-        pushHomeViewController()
-    }
-    
-    @IBAction func touchRegisterButtonActionHandler(_ sender: UIButton) {
-        pushHomeViewController()
-    }
-    
-    // MARK: - private functions
-    
-    private func pushHomeViewController() {
-        let storyboard = UIStoryboard(name: "Home", bundle: .main)
-        let homeViewController = storyboard.instantiateViewController(withIdentifier: "Home") as! HomeViewController
-        
-        navigationController?.pushViewController(homeViewController, animated: true)
-    }
 }
