58ef6d357d56960365705648709ea022c843821d
Bernard Ibrovic <ibrovic.bernard558@gmail.com>
2021-07-21 23:56:12 +0200

    Add login and register API calls

--- TVShows/TVShows/Login/LoginViewController.swift
+++ TVShows/TVShows/Login/LoginViewController.swift
@@ -6,8 +6,20 @@
 //
 
 import UIKit
+import Alamofire
+import SVProgressHUD
 
 final class LoginViewController: UIViewController {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet private weak var emailTextField: AuthTextField!
+    @IBOutlet private weak var passwordTextField: AuthTextField!
+    
+    // MARK: - Properties
+    private var currentUser: User?
+    private var configuration = URLSessionConfiguration.af.default
+    private lazy var session = Session(configuration: configuration)
     
     
     // MARK: - Lifecycle methods
@@ -30,12 +42,48 @@ final class LoginViewController: UIViewController {
     // MARK: - private functions
     
     @IBAction private func touchLoginButtonActionHandler(_ sender: UIButton) {
-        pushHomeViewController()
+        if emailTextField.text!.isEmpty || passwordTextField.text!.isEmpty {
+            print("Please fill in all of the required fields")
+            return
+        }
+        
+        SVProgressHUD.show()
+        session.request(UserRouter.login(email: emailTextField.text!, password: passwordTextField.text!)).validate().responseDecodable(of: UserResponse.self) {
+            [weak self] dataResponse in
+            SVProgressHUD.dismiss()
+            switch dataResponse.result {
+            case .success(let user):
+                self?.currentUser = user.user
+                self?.pushHomeViewController()
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
     
     @IBAction private func touchRegisterButtonActionHandler(_ sender: UIButton) {
-        pushHomeViewController()
+        
+        if emailTextField.text!.isEmpty || passwordTextField.text!.isEmpty {
+            print("Please fill in all of the required fields")
+            return
+        }
+        
+        SVProgressHUD.show()
+        session.request(UserRouter.register(email: emailTextField.text!, password: passwordTextField.text!)).validate().responseDecodable(of: UserResponse.self) {
+            [weak self] dataResponse in
+            SVProgressHUD.dismiss()
+            switch dataResponse.result {
+            case .success(let user):
+                self?.currentUser = user.user
+                self?.pushHomeViewController()
+            case .failure(let error):
+                print(error)
+            }
+        }
+        
     }
+        
+        
     
     private func pushHomeViewController() {
         let storyboard = UIStoryboard(name: "Home", bundle: .main)
