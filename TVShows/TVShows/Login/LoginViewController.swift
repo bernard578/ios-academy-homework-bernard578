ee6ea2a11decfa9d869cff006b8f3fab7b02b443
Bernard Ibrovic <ibrovic.bernard558@gmail.com>
2021-07-21 21:43:59 +0200

    Add Home.storyboard and HomeViewController

--- TVShows/TVShows/Login/LoginViewController.swift
+++ TVShows/TVShows/Login/LoginViewController.swift
@@ -18,7 +18,7 @@ final class LoginViewController: UIViewController {
     
     // MARK: - Actions
     
-    @IBAction func touchRememberMeButtonActionHandler(_ sender: UIButton) {
+    @IBAction private func touchRememberMeButtonActionHandler(_ sender: UIButton) {
         if sender.isSelected {
             sender.setImage(UIImage(named: "ic-checkbox-unselected"), for: .normal)
         } else {
@@ -26,4 +26,23 @@ final class LoginViewController: UIViewController {
         }
         sender.isSelected = !sender.isSelected
     }
+    
+    // MARK: - private functions
+    
+    @IBAction private func touchLoginButtonActionHandler(_ sender: UIButton) {
+        pushHomeViewController()
+    }
+    
+    @IBAction private func touchRegisterButtonActionHandler(_ sender: UIButton) {
+        pushHomeViewController()
+    }
+    
+    private func pushHomeViewController() {
+        let storyboard = UIStoryboard(name: "Home", bundle: .main)
+        let homeViewController = storyboard.instantiateViewController(withIdentifier: "HomeViewController") as! HomeViewController
+        
+        navigationController?.pushViewController(homeViewController, animated: true
+        )
+    }
+    
 }
