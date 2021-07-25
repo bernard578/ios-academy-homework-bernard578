3572957e649f556ba15c1c7e1fa8ffe1277ca11e
Bernard Ibrovic <ibrovic.bernard558@gmail.com>
2021-07-26 01:32:57 +0200

    Add APIManager

--- TVShows/TVShows/Login/LoginViewController.swift
+++ TVShows/TVShows/Login/LoginViewController.swift
@@ -6,91 +6,105 @@
 //
 
 import UIKit
-import Alamofire
 import SVProgressHUD
 
 final class LoginViewController: UIViewController {
     
     // MARK: - Outlets
     
-    @IBOutlet private weak var emailTextField: AuthTextField!
-    @IBOutlet private weak var passwordTextField: AuthTextField!
+    @IBOutlet weak var rememberMeButton: UIButton!
+    @IBOutlet private weak var emailTextField: BottomLinedTextField!
+    @IBOutlet private weak var passwordTextField: BottomLinedTextField!
     
     // MARK: - Properties
-    private var currentUser: User?
-    private var configuration = URLSessionConfiguration.af.default
-    private lazy var session = Session(configuration: configuration)
     
+    private var currentUser: User?
+    private var manager = APIManager()
     
     // MARK: - Lifecycle methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setRememberMeButtonImages()
     }
     
     // MARK: - Actions
     
     @IBAction private func touchRememberMeButtonActionHandler(_ sender: UIButton) {
-        if sender.isSelected {
-            sender.setImage(UIImage(named: "ic-checkbox-unselected"), for: .normal)
-        } else {
-            sender.setImage(UIImage(named: "ic-checkbox-selected"), for: . normal)
-        }
+        
         sender.isSelected = !sender.isSelected
     }
-    
-    // MARK: - private functions
-    
+        
     @IBAction private func touchLoginButtonActionHandler(_ sender: UIButton) {
-        if emailTextField.text!.isEmpty || passwordTextField.text!.isEmpty {
-            print("Please fill in all of the required fields")
+        guard
+            let email = emailTextField.text,
+            let password = passwordTextField.text,
+            !email.isEmpty,
+            !password.isEmpty
+        else {
+            print("Login error")
             return
         }
         
         SVProgressHUD.show()
-        session.request(UserRouter.login(email: emailTextField.text!, password: passwordTextField.text!)).validate().responseDecodable(of: UserResponse.self) {
-            [weak self] dataResponse in
+        manager.makeLoginRequest(email: email, password: password) {
+        [weak self] dataResponse in
             SVProgressHUD.dismiss()
-            switch dataResponse.result {
+            switch dataResponse {
             case .success(let user):
                 self?.currentUser = user.user
                 self?.pushHomeViewController()
+                print("ovo je ok iz login")
+                print(self?.currentUser)
             case .failure(let error):
                 print(error)
             }
         }
+        
     }
     
     @IBAction private func touchRegisterButtonActionHandler(_ sender: UIButton) {
-        
-        if emailTextField.text!.isEmpty || passwordTextField.text!.isEmpty {
-            print("Please fill in all of the required fields")
+        guard
+            let email = emailTextField.text,
+            let password = passwordTextField.text,
+            !email.isEmpty,
+            !password.isEmpty
+        else {
+            print("Registration error")
             return
         }
         
         SVProgressHUD.show()
-        session.request(UserRouter.register(email: emailTextField.text!, password: passwordTextField.text!)).validate().responseDecodable(of: UserResponse.self) {
-            [weak self] dataResponse in
+        manager.makeRegisterRequest(email: email, password: password) {
+        [weak self] dataResponse in
             SVProgressHUD.dismiss()
-            switch dataResponse.result {
+            switch dataResponse {
             case .success(let user):
                 self?.currentUser = user.user
                 self?.pushHomeViewController()
+                print("ovo je ok iz register")
+                print(self?.currentUser)
             case .failure(let error):
                 print(error)
             }
         }
-        
+    }
+}
+
+// MARK: - private functions
+
+private extension LoginViewController {
+    
+    func setRememberMeButtonImages() {
+        rememberMeButton.setImage(UIImage(named: "ic-checkbox-unselected"), for: .normal)
+        rememberMeButton.setImage(UIImage(named: "ic-checkbox-selected"), for: . selected)
     }
         
-        
-    
-    private func pushHomeViewController() {
+    func pushHomeViewController() {
         let storyboard = UIStoryboard(name: "Home", bundle: .main)
         let homeViewController = storyboard.instantiateViewController(withIdentifier: "HomeViewController") as! HomeViewController
         
         navigationController?.pushViewController(homeViewController, animated: true
         )
     }
-    
 }
--- TVShows/TVShows/Networking/Routers/UserRouter.swift
+++ TVShows/TVShows/Networking/Routers/UserRouter.swift
@@ -9,7 +9,7 @@ import Foundation
 import Alamofire
 
 enum UserRouter: URLRequestConvertible {
-        case login(email: String, password: String)
+    case login(email: String, password: String)
     case register(email: String, password: String)
     
     var path: String {
@@ -45,14 +45,17 @@ enum UserRouter: URLRequestConvertible {
         }
     }
     
+    // MARK: - private functions
     
     func asURLRequest() throws -> URLRequest {
-        let url = try URL(string: Constants.baseURL.asURL().appendingPathComponent(path).absoluteString.removingPercentEncoding!)
+        let url = try URL(string: Constants.baseURL
+                            .asURL()
+                            .appendingPathComponent(path)
+                            .absoluteString.removingPercentEncoding!)
         print(url)
         var request = URLRequest.init(url: url!)
         request.httpMethod = method.rawValue
         request.timeoutInterval = TimeInterval(10*1000)
         return try URLEncoding.default.encode(request, with: parameters)
     }
-    
 }
